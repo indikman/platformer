17d8f83ff61a93b6ec86bde05f3e4f150f989a7e
Indika Wijesooriya <indikman@gmail.com>
2024-02-20 23:18:06 -0700

    Updated the character data scriptable obj to clear the dictionary on disabled

--- Assets/Platformer/Scripts/CharacterDataScriptableObject.cs
+++ Assets/Platformer/Scripts/CharacterDataScriptableObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEditor.U2D.Animation;
 using UnityEngine;
@@ -26,21 +27,28 @@ namespace Spatialminds.Platformer
 
             foreach (var anim in characterAnimatorController.parameters)
             {
-                if(animatorParameters.TryAdd(anim.name, anim.nameHash)) Debug.Log("Added param " + anim.name);
-                else Debug.LogWarning("Param " + anim.name + " already exists. Not adding it again!");
+                if(!animatorParameters.TryAdd(anim.name, anim.nameHash)) Debug.LogWarning("Param " + anim.name + " already exists. Not adding it again!");
             }
 
-            Debug.Log("All the Params added");
-        }
-
-        void OnEnable()
-        {
-            UpdateAnimatorParameters();
+            //Debug.Log("All the Params added");
         }
 
         public void ClearAnimParams()
         {
             animatorParameters.Clear();
         }
+
+
+        void OnEnable()
+        {
+            UpdateAnimatorParameters();
+        }
+
+        void OnDisable()
+        {
+            ClearAnimParams();
+        }
+
+        
     }
 }
